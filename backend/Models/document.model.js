6bd9446c3fe007116d494679cda0cfe3c545c513
itachi-47 <varunpahuja2005@gmail.com>
2026-03-19 22:18:27 +0530

    removed og's file

--- backend/Models/document.model.js
+++ backend/Models/document.model.js
@@ -26,7 +26,7 @@ const documentSchema = new mongoose.Schema(
                 "income_certificate",
                 "domaicile",
                 "jee_result",
-                "other",            // Signature
+                "other",            
             ],
             required: true,
         },
